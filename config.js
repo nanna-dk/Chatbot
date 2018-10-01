bc1489503ea582a3168a068f2c83bbc2764b387c
cdq217 <nel@adm.ku.dk>
2018-10-01 13:59:35 +0200

    Config vars

--- config.js
+++ config.js
@@ -8,18 +8,5 @@ module.exports = {
     DF_LANGUAGE_CODE: process.env.DF_LANGUAGE_CODE,
     GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
     GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    SENGRID_API_KEY: process.env.SENGRID_API_KEY,
-    EMAIL_FROM: process.env.EMAIL_FROM,
-    EMAIL_TO: process.env.EMAIL_TO,
-    WEATHER_API_KEY: process.env.WEATHER_API_KEY,
-    ADMIN_ID: process.env.ADMIN_ID,
-    PG_CONFIG: {
-        user: process.env.PG_CONFIG_USER,
-        database: process.env.PG_CONFIG_DATABASE,
-        password: process.env.PG_CONFIG_PASSWORD,
-        host: process.env.PG_CONFIG_HOST,
-        port: 5432,
-        max: 10,
-        idleTimeoutMillis: 30000,
-    }
+    ADMIN_ID: process.env.ADMIN_ID
 };
